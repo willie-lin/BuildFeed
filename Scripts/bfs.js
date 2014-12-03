b35447fdf788529772ff36244cef4b81882018fc
Thomas Hounsell <commits@thounsell.co.uk>
2014-12-03 11:52:03 +0000

    Timeouts and aborts for BuildFeed Search

--- Scripts/bfs.js
+++ Scripts/bfs.js
@@ -1,15 +1,31 @@
-﻿$(function () {
+﻿var bfsAjax;
+var bfsTimeout;
+
+$(function () {
     $("#search-input").keyup(function () {
         var search = $(this);
         $(this).parent().find(".list-group").remove();
-        $.ajax("/api/GetSearchResult/?query=" + $(this).val()).done(function (data) {
-            var template = $.templates("#result-template");
-            var content = $("<div class='list-group'></div>");
 
-            var item = template.render(data);
-            content.append(item);
+        if (typeof bfsTimeout != 'undefined')
+        {
+            clearTimeout(bfsTimeout);
+        }
 
-            search.after(content);
-        });
+        if (typeof bfsAjax != 'undefined' && bfsAjax.readyState != 4)
+        {
+            bfsAjax.abort();
+        }
+
+        bfsTimeout = setTimeout(function (object) {
+            bfsAjax = $.ajax("/api/GetSearchResult/?query=" + $("#search-input").val()).done(function (data) {
+                var template = $.templates("#result-template");
+                var content = $("<div class='list-group'></div>");
+
+                var item = template.render(data);
+                content.append(item);
+
+                search.after(content);
+            });
+        }, 200);
     });
 });
