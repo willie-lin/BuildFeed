4eb41c40e83ea4483ac6d4ab593ea0ba87e2f6a1
Thomas Hounsell <commits@thounsell.co.uk>
2015-03-28 00:39:53 +0000

    This branch is fucked
    Abandon ship.

--- Scripts/index-page.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="typings/jquery/jquery.d.ts" />
-/// <reference path="typings/jsrender/jsrender.d.ts" />
-var IndexPage = (function () {
-    function IndexPage() {
-        this.panelSelector = "#expand-panel";
-    }
-    return IndexPage;
-})();
-//# sourceMappingURL=index-page.js.map
