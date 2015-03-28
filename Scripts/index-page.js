167afdc41bb58f82bd72810d397e94fbb0a774a2
Thomas Hounsell <commits@thounsell.co.uk>
2015-03-28 00:20:31 +0000

    Refresh Pt 3a

---
+++ Scripts/index-page.js
@@ -0,0 +1,9 @@
+/// <reference path="typings/jquery/jquery.d.ts" />
+/// <reference path="typings/jsrender/jsrender.d.ts" />
+var IndexPage = (function () {
+    function IndexPage() {
+        this.panelSelector = "#expand-panel";
+    }
+    return IndexPage;
+})();
+//# sourceMappingURL=index-page.js.map
--- Scripts/index-page.ts
+++ Scripts/index-page.ts
@@ -1,1 +1,6 @@
-﻿
+﻿/// <reference path="typings/jquery/jquery.d.ts" />
+/// <reference path="typings/jsrender/jsrender.d.ts" />
+
+class IndexPage {
+    private panelSelector: string = "#expand-panel";
+}
