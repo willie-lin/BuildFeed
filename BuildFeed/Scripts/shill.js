62cc5f34ea96b2b77ec19938d63eea047c6194b7
Thomas Hounsell <commit@thounsell.co.uk>
2016-06-23 19:21:01 +0100

    Revert "BuildFeed shills for #Brexit :uk:"
    This reverts commit b72788f6f3fc2bc43c00d547b12ca9ddc617c3f8.

--- BuildFeed/Scripts/shill.js
+++
@@ -1,8 +0,0 @@
-﻿$(function() {
-   $(".shill-eu-close")
-      .click(function(event) {
-         event.preventDefault();
-         $(".shill-eu").hide();
-         document.cookie = "no_shill=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
-      });
-})
