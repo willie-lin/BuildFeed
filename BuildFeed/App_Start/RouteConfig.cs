ccb4e3d02474724eeee6c1ca44e97eb06e4580ff
Thomas Hounsell <commit@thounsell.co.uk>
2016-07-13 12:35:21 +0100

    RSS Library swap

--- BuildFeed/App_Start/RouteConfig.cs
+++ BuildFeed/App_Start/RouteConfig.cs
@@ -9,14 +9,14 @@ namespace BuildFeed
       public static void RegisterRoutes(RouteCollection routes)
       {
          routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-         routes.MapHttpRoute("API", "api/{action}/{id}",
-             new
-             {
-                controller = "api",
-                action = "GetBuilds",
-                id = UrlParameter.Optional
-             });
+
          routes.AppendTrailingSlash = true;
+         routes.MapHttpRoute("API", "api/{action}/{id}", new
+         {
+            controller = "api",
+            action = "GetBuilds",
+            id = UrlParameter.Optional
+         });
          routes.MapMvcAttributeRoutes();
       }
    }
--- BuildFeed/Global.asax.cs
+++ BuildFeed/Global.asax.cs
@@ -6,7 +6,7 @@ using System.Web.Routing;
 
 namespace BuildFeed
 {
-   public class MvcApplication : System.Web.HttpApplication
+   public class MvcApplication : HttpApplication
    {
       protected void Application_Start()
       {
