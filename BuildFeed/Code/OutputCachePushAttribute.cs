9100ba667372f3cafbd176cc096accd604b2b7f3
Thomas Hounsell <commit@thounsell.co.uk>
2016-08-21 21:49:38 +0100

    Add HTTP/2 Push to Output Cache

---
+++ BuildFeed/Code/OutputCachePushAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BuildFeed.Code.Options;
+
+namespace BuildFeed.Code
+{
+   public class OutputCachePushAttribute: ActionFilterAttribute
+   {
+      public override void OnResultExecuted(ResultExecutedContext filterContext)
+      {
+         bool isRtl = CultureInfo.CurrentUICulture.TextInfo.IsRightToLeft;
+         Theme theme = new Theme(Theme.DetectTheme(filterContext.HttpContext));
+
+         filterContext.HttpContext.Response.PushPromise("/res/css/default.min.css");
+         filterContext.HttpContext.Response.PushPromise(VirtualPathUtility.ToAbsolute(theme.CssPath));
+         if (isRtl)
+         {
+            filterContext.HttpContext.Response.PushPromise("/res/css/rtl.min.css");
+         }
+         filterContext.HttpContext.Response.PushPromise("/res/ts/bfs.min.js");
+      }
+   }
+}
