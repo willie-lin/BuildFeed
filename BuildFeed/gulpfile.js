15de87c91b0cf84fe9d9d15e42cc5169f9cf8ef7
Thomas Hounsell <commits@thounsell.co.uk>
2016-12-03 00:02:43 +0000

    Rmove last BundleConfig parts, move fully to gulp.

--- BuildFeed/gulpfile.js
+++ BuildFeed/gulpfile.js
@@ -3,6 +3,9 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var cleanCss = require("gulp-clean-css");
 var sourceMaps = require("gulp-sourcemaps");
+var ts = require("gulp-typescript");
+var uglify = require("gulp-uglify");
+var autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("sass-compile",
     function ()
@@ -10,13 +13,34 @@ gulp.task("sass-compile",
         gulp.src("./res/css/*.scss")
             .pipe(sourceMaps.init())
             .pipe(sass())
+            .pipe(autoprefixer({
+                browsers: ["> 1% in GB", "IE 8"],
+                cascade: false
+            }))
             .pipe(cleanCss())
             .pipe(sourceMaps.write("./"))
             .pipe(gulp.dest("./res/css/"));
+    });
+
+gulp.task("typescript",
+    function ()
+    {
+        var result = gulp.src("./res/ts/*.ts")
+            .pipe(sourceMaps.init())
+            .pipe(ts({
+                target: "es5",
+                sourceMap: false
+            }));
+
+        return result.js
+            .pipe(uglify())
+            .pipe(sourceMaps.write("./"))
+            .pipe(gulp.dest("./res/ts/"));
     });
 
 gulp.task("watch-sass",
     function ()
     {
         gulp.watch("./res/scss/*.scss", ["sass-compile"]);
+        gulp.watch("./res/ts/*.ts", ["typescript"]);
     });
